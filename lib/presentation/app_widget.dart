1eab886a4fd790665b40458a9b834aec4add4652
Miguel Alvarez Lesmes <alvarez.lesmes.miguel@gmail.com>
2022-09-22 23:05:39 +0200

    add material app router

--- lib/presentation/app_widget.dart
+++ lib/presentation/app_widget.dart
@@ -1,15 +1,21 @@
-import 'package:complete_flutter_project_starter/presentation/home/home_page.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:complete_flutter_project_starter/presentation/routes/routes.gr.dart';
 import 'package:flutter/material.dart';
 
 class AppWidget extends StatelessWidget {
-  const AppWidget({Key? key}) : super(key: key);
+  final _appRouter = AppRouter();
+
+  AppWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'App Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const HomePage(),
+      routerDelegate: _appRouter.delegate(
+        navigatorObservers: () => [HeroController(), AutoRouteObserver()],
+      ),
+      routeInformationParser: _appRouter.defaultRouteParser(),
     );
   }
 }
