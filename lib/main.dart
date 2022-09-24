877eaaf304ff3b0d50684ca99f9027a709bec780
Miguel Alvarez Lesmes <alvarez.lesmes.miguel@gmail.com>
2022-09-24 18:07:31 +0200

    fix issue with project name & upgrade gradle version

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,5 @@ import 'package:complete_flutter_project_starter/presentation/app_widget.dart';
 import 'package:flutter/widgets.dart';
 
 Future<void> startApp(Env env) async {
-  runApp(const AppWidget());
+  runApp(AppWidget());
 }
